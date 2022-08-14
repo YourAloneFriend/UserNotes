c564e78c3a8da743e74c6dd558c4e203643b0b0e
YourAloneFriend <vasya202003@gmail.com>
2022-08-14 23:12:06 +0300

    User and Note models done.

--- src/main/java/com/application/model/Note.java
+++ src/main/java/com/application/model/Note.java
@@ -1,7 +1,75 @@
 package com.application.model;
 
+import java.util.Objects;
+
 /***
  *   Note module(class) with data fields and some methods.
  */
 public class Note {
+
+    private String noteName;
+    private String note;
+    private Long userId;
+
+    public String getNoteName() {
+        return noteName;
+    }
+
+    public void setNoteName(String noteName) {
+        this.noteName = noteName;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Note note1 = (Note) o;
+        return Objects.equals(noteName, note1.noteName) && Objects.equals(note, note1.note) && Objects.equals(userId, note1.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(noteName, note, userId);
+    }
+
+    public static NoteBuilder base() {return new NoteBuilder();}
+
+    public static class NoteBuilder{
+        private Note noteBuilder;
+
+        private NoteBuilder() {noteBuilder = new Note();}
+
+        public NoteBuilder noteName(String noteName) {
+            this.noteBuilder.noteName = noteName;
+            return this;
+        }
+
+        public NoteBuilder note(String note) {
+            this.noteBuilder.note = note;
+            return this;
+        }
+
+        public NoteBuilder userId(Long userId) {
+            this.noteBuilder.userId = userId;
+            return this;
+        }
+
+        public Note build() {return noteBuilder;}
+    }
 }
--- src/main/java/com/application/model/User.java
+++ src/main/java/com/application/model/User.java
@@ -1,7 +1,89 @@
 package com.application.model;
 
+import java.util.Objects;
+import java.util.regex.Pattern;
+
 /***
  *   User module(class) with data fields and some methods.
  */
 public class User {
+    private String userName;
+    private String email;
+    private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) throws IllegalArgumentException{
+        checkEmailValidation(email);
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) throws IllegalArgumentException{
+        checkPasswordValidation(password);
+        this.password = password;
+    }
+
+    private static void checkEmailValidation(String email) {
+        if(Pattern.matches("^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$", email))
+            throw new IllegalArgumentException("Email isn't valid!");
+    }
+
+    private static void checkPasswordValidation(String password) {
+        if(Pattern.matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,32}$", password))
+            throw new IllegalArgumentException("Password isn't valid!");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(userName, user.userName) && Objects.equals(email, user.email) && Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userName, email, password);
+    }
+
+    public static UserBuilder base(){return new UserBuilder();}
+
+    public static class UserBuilder{
+        private User user;
+
+        private UserBuilder() {user = new User();}
+
+        public UserBuilder userName(String userName){
+            this.user.userName = userName;
+            return this;
+        }
+
+        public UserBuilder email(String email){
+            checkEmailValidation(email);
+            this.user.email = email;
+            return this;
+        }
+
+        public UserBuilder password(String password){
+            checkPasswordValidation(password);
+            this.user.password = password;
+            return this;
+        }
+
+        public User build() {return user;}
+    }
 }
