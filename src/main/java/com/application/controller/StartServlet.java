bc4c84cb57a277f5243c7dfba314e5e9510b8d5e
YourAloneFriend <vasya202003@gmail.com>
2022-08-16 01:26:02 +0300

    Start servlet and .jsp file are completed with code. main.css added some styles.

--- src/main/java/com/application/controller/StartServlet.java
+++ src/main/java/com/application/controller/StartServlet.java
@@ -1,7 +1,12 @@
 package com.application.controller;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  *      Servlet where is greetings to user and are two buttons (SignUp/SignIn).
@@ -9,5 +14,13 @@ import javax.servlet.http.HttpServlet;
 
 @WebServlet("/")
 public class StartServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException{
+        RequestDispatcher requestDispatcher = httpServletRequest.getRequestDispatcher("/WEB-INF/pages/start.jsp");
+        requestDispatcher.forward(httpServletRequest, httpServletResponse);
+    }
+
+    protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
+
+    }
 }
 
--- src/main/webapp/WEB-INF/pages/start.jsp
+++ src/main/webapp/WEB-INF/pages/start.jsp
@@ -1,9 +1,23 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Title</title>
+    <title>UserNotes</title>
+
+    <style>
+        <%@include file="../styles/main.css"%>
+    </style>
 </head>
 <body>
+<div class="header-title color">
+    <h1>Hello and welcome to UserNotes project!</h1>
+    <h2>SignIn or SignUp, if you don't have an account, and let's go.</h2>
+</div>
 
+<form action="/registration">
+    <center><button type="submit" class="big-btn color">SignUp</button></center>
+</form>
+<form action="/authorization">
+    <center><button type="submit" class="big-btn color">SignIn</button></center>
+</form>
 </body>
 </html>
